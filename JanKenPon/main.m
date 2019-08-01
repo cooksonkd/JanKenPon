bb7a36be55e80ab8b82933b699e4463b23ecf851
Keenan Cookson <kenaan.cookson@zapper.com>
2019-08-01 15:52:59 +0200

    Initial Commit

---
+++ JanKenPon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JanKenPon
+//
+//  Created by Keenan Cookson on 2019/08/01.
+//  Copyright © 2019 Keenan Cookson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
